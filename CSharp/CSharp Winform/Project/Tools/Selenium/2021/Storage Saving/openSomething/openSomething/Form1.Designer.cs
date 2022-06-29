ca971076ba5c9bf51ea2e06683b8063b8f74eb52
Ha Duc <haducvcvb@gmail.com>
2022-06-29 18:32:28 +0700

    commit CSharp / Winform - Project - Tools - Selenium - 2021 v1| 29/06/2022

---
+++ CSharp/CSharp Winform/Project/Tools/Selenium/2021/Storage Saving/openSomething/openSomething/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace openSomething
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnYoutube = new System.Windows.Forms.Button();
+            this.btnFacebok = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(25, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Url";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtUrl.Location = new System.Drawing.Point(69, 17);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.PlaceholderText = "https://www.example.com/";
+            this.txtUrl.Size = new System.Drawing.Size(416, 34);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.TextChanged += new System.EventHandler(this.textChangeBoth);
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.Location = new System.Drawing.Point(25, 205);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(460, 57);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(265, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 57);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(25, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 57);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnYoutube
+            // 
+            this.btnYoutube.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnYoutube.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnYoutube.Location = new System.Drawing.Point(25, 76);
+            this.btnYoutube.Name = "btnYoutube";
+            this.btnYoutube.Size = new System.Drawing.Size(220, 57);
+            this.btnYoutube.TabIndex = 7;
+            this.btnYoutube.Text = "Youtube";
+            this.btnYoutube.UseVisualStyleBackColor = true;
+            this.btnYoutube.Click += new System.EventHandler(this.btnYoutube_Click);
+            // 
+            // btnFacebok
+            // 
+            this.btnFacebok.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFacebok.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnFacebok.Location = new System.Drawing.Point(265, 76);
+            this.btnFacebok.Name = "btnFacebok";
+            this.btnFacebok.Size = new System.Drawing.Size(220, 57);
+            this.btnFacebok.TabIndex = 8;
+            this.btnFacebok.Text = "Facebook";
+            this.btnFacebok.UseVisualStyleBackColor = true;
+            this.btnFacebok.Click += new System.EventHandler(this.btnFacebok_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 343);
+            this.Controls.Add(this.btnFacebok);
+            this.Controls.Add(this.btnYoutube);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUrl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Open Something!";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnYoutube;
+        private System.Windows.Forms.Button btnFacebok;
+    }
+}
+
---
+++ CSharp/CSharp Winform/Project/Tools/Selenium/2021/Storage Saving/openSomething/openSomething/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace openSomething
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            currentSetting();
+        }
+
+        ChromeDriver driver;
+        Thread thr;
+
+        private void textChangeBoth(object sender, EventArgs e)
+        {
+            //txtBoth(url + tk)
+            if (txtUrl.Text.Trim() != "")
+            {
+                btnStart.ForeColor = Color.Lime;
+                btnStart.Cursor = Cursors.Hand;
+            }
+            else
+            {
+                btnStart.ForeColor = Color.Red;
+                btnStart.Cursor = Cursors.No;
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (btnStart.Cursor == Cursors.Hand)
+            {
+                thr = new Thread(letStart);
+                thr.Start();
+            }
+        }
+
+        private void currentSetting()
+        {
+            btnStart.ForeColor = Color.Red;
+            btnStart.Cursor = Cursors.No;
+        }
+
+        private void currentSettingReady()
+        {
+            btnStart.ForeColor = Color.Lime;
+            btnStart.UseWaitCursor = false;
+            btnStart.Text = "Send";
+            btnStart.Focus();
+        }
+
+        private void letStart()
+        {
+            btnStart.ForeColor = Color.Gold;
+            btnStart.UseWaitCursor = true;
+            btnStart.Text = "Starting...";
+            opSelenium(); // chay browser
+            Thread.Sleep(1000);
+
+            //reset
+            currentSettingReady();
+            Thread.Sleep(1500);
+        }
+
+        private void opSelenium()
+        {
+            try
+            {
+                ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+                var checkConsole = service.HideCommandPromptWindow = false; //hide console
+
+                driver = new ChromeDriver(service);
+                openIncognito();
+
+                driver.Navigate().GoToUrl(txtUrl.Text.Trim());
+                Thread.Sleep(1000);
+                //driver.FindElement(By.XPath("//button[@class='ytp-large-play-button ytp-button']")).Click();
+
+                Thread.Sleep(2000);
+                //Maximize Browser  
+                //driver.Manage().Window.Maximize();
+                driver.Manage().Window.Minimize();
+                Thread.Sleep(10000);
+                driver.Navigate().GoToUrl("https://clubloli.com/v/20675/");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clSelenium()
+        {
+            if (driver != null)
+            {
+                driver.Dispose();
+                driver.Quit();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            clSelenium();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clSelenium();
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clSelenium();
+        }
+
+        private void btnYoutube_Click(object sender, EventArgs e)
+        {
+            txtUrl.Text = "https://youtube.com/";
+        }
+
+        private void btnFacebok_Click(object sender, EventArgs e)
+        {
+            txtUrl.Text = "https://www.facebook.com/";
+        }
+
+        private void openIncognito()
+        {
+            SendKeys.SendWait("^+{n}");
+            driver.Close(); //close tab hien tai
+            driver.SwitchTo().Window(driver.WindowHandles[driver.WindowHandles.Count - 1]); //chuyen chrDrive sang tab Incognito
+        }
+    }
+}
