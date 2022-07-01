953f91d7442895e28df2b65c4d315172d9a0e6cd
Ha Duc <haducvcvb@gmail.com>
2022-07-01 09:39:45 +0700

    commit CSharp / Winform - Project - Tools - Selenium - 2021 v2| 29/06/2022

---
+++ CSharp/CSharp Winform/Project/Tools/Selenium/2021/Open Chrome with Users Data/Selenium 2/Selenium 2/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Selenium_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnStart.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnStart.FlatAppearance.CheckedBackColor = System.Drawing.Color.Fuchsia;
+            this.btnStart.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow;
+            this.btnStart.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.Location = new System.Drawing.Point(12, 21);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(345, 78);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnStop.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnStop.FlatAppearance.CheckedBackColor = System.Drawing.Color.Fuchsia;
+            this.btnStop.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow;
+            this.btnStop.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStop.Location = new System.Drawing.Point(12, 116);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(345, 78);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnStart;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 214);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ CSharp/CSharp Winform/Project/Tools/Selenium/2021/Open Chrome with Users Data/Selenium 2/Selenium 2/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace Selenium_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        ChromeDriver driver;
+        ChromeOptions options;
+
+        Thread thr;
+
+        //Path
+       // string ProfileFolderPath = Application.StartupPath + "Profile";
+
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            closeDriver();
+            Application.Exit();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //closeDriver();
+            thr = new Thread(strartProfile);
+            thr.Start();
+            
+        }
+
+        private void closeDriver()
+        {
+            //neu co driver -> thoat driver
+            if (driver != null)
+            {
+                try
+                {
+                    driver.Dispose();
+                    driver.Quit();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+
+        private void strartProfile()
+        {
+            //create ChromeOptions options;
+            options = new ChromeOptions();
+
+            //create folder
+            if (!Directory.Exists(ProfileFolderPath))
+            {
+                //tao folder moi
+                Directory.CreateDirectory(ProfileFolderPath);
+            }
+
+            if (Directory.Exists(ProfileFolderPath))
+            {
+                int count = 1;
+                options.AddArgument("user-data-dir=" + ProfileFolderPath + "\\User_" + count);
+                options.AddArgument(@"profile-directory=" + "User_" + count); //chose profile
+            }
+
+            driver = new ChromeDriver(options);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            closeDriver();
+        }
+    }
+}
