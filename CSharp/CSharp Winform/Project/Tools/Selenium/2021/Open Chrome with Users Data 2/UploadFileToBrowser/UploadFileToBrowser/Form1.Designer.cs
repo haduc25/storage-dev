ecc49b24aab998b2b9951814af43e8f4c77816b0
Ha Duc <haducvcvb@gmail.com>
2022-07-01 09:50:46 +0700

    commit CSharp / Winform - Project - Tools - Selenium - 2021 v3 | 01/07/2022

---
+++ CSharp/CSharp Winform/Project/Tools/Selenium/2021/Open Chrome with Users Data 2/UploadFileToBrowser/UploadFileToBrowser/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace UploadFileToBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatAppearance.BorderSize = 5;
+            this.btnStart.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Algerian", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.Location = new System.Drawing.Point(48, 91);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(478, 91);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.FlatAppearance.BorderSize = 5;
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Algerian", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnClose.Location = new System.Drawing.Point(48, 236);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(478, 91);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 375);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UploadToBrowser";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ CSharp/CSharp Winform/Project/Tools/Selenium/2021/Open Chrome with Users Data 2/UploadFileToBrowser/UploadFileToBrowser/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace UploadFileToBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ChromeDriver driver;
+        ChromeOptions options;
+        Thread thr;
+
+        //Path
+        string ProfileFolderPath = Application.StartupPath + "Profile";
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            closeBrowser();
+            thr = new Thread(startBrowser);
+            thr.Start();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            closeBrowser();
+            Application.Exit();
+        }
+
+        private void closeBrowser()
+        {
+            //neu co driver -> thoat driver
+            if (driver != null)
+            {
+                try
+                {
+                    driver.Dispose();
+                    driver.Quit();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            closeBrowser();
+        }
+
+        private void startBrowser()
+        {
+            //create ChromeOptions options;
+            options = new ChromeOptions();
+
+            //create folder
+            if (!Directory.Exists(ProfileFolderPath))
+            {
+                //tao folder moi
+                Directory.CreateDirectory(ProfileFolderPath);
+            }
+
+            if (Directory.Exists(ProfileFolderPath))
+            {
+                int count = 1;
+                //MessageBox.Show(ProfileFolderPath + "\\Profile 1");
+                options.AddArgument("user-data-dir=" + ProfileFolderPath + "\\User_" + count);
+                options.AddArgument(@"profile-directory=" + "\\User_" + count); //chose profile
+            }
+
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+        }
+
+
+    }
+}
