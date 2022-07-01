5eba2c59e70790e81dfd021e4153c2ba602fedb0
Ha Duc <haducvcvb@gmail.com>
2022-07-01 10:39:03 +0700

    commit CSharp / Winform - Project - Tools - Selenium - 2022 v2 | 01/07/2022

---
+++ CSharp/CSharp Winform/Project/Tools/Selenium/2022/Valorant NM/Main version/NightMarketVal/NightMarketVal/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace NightMarketVal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAcc = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbAcc = new System.Windows.Forms.Label();
+            this.lbPass = new System.Windows.Forms.Label();
+            this.txtPercent = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAcc
+            // 
+            this.btnAcc.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAcc.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAcc.Location = new System.Drawing.Point(12, 100);
+            this.btnAcc.Name = "btnAcc";
+            this.btnAcc.Size = new System.Drawing.Size(199, 49);
+            this.btnAcc.TabIndex = 0;
+            this.btnAcc.Text = "Tài khoản";
+            this.btnAcc.UseVisualStyleBackColor = false;
+            this.btnAcc.Click += new System.EventHandler(this.btnAcc_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(223, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Mật khẩu";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File name";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(199, 49);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lbAcc
+            // 
+            this.lbAcc.AutoSize = true;
+            this.lbAcc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAcc.Location = new System.Drawing.Point(12, 65);
+            this.lbAcc.Name = "lbAcc";
+            this.lbAcc.Size = new System.Drawing.Size(45, 20);
+            this.lbAcc.TabIndex = 4;
+            this.lbAcc.Text = "File1";
+            // 
+            // lbPass
+            // 
+            this.lbPass.AutoSize = true;
+            this.lbPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPass.Location = new System.Drawing.Point(219, 65);
+            this.lbPass.Name = "lbPass";
+            this.lbPass.Size = new System.Drawing.Size(45, 20);
+            this.lbPass.TabIndex = 5;
+            this.lbPass.Text = "File2";
+            // 
+            // txtPercent
+            // 
+            this.txtPercent.Location = new System.Drawing.Point(370, 34);
+            this.txtPercent.MaxLength = 2;
+            this.txtPercent.Name = "txtPercent";
+            this.txtPercent.Size = new System.Drawing.Size(52, 22);
+            this.txtPercent.TabIndex = 6;
+            this.txtPercent.Click += new System.EventHandler(this.txtPercent_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(304, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Sort %";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(223, 162);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(199, 49);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 223);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPercent);
+            this.Controls.Add(this.lbPass);
+            this.Controls.Add(this.lbAcc);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAcc);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAcc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lbAcc;
+        private System.Windows.Forms.Label lbPass;
+        private System.Windows.Forms.TextBox txtPercent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ CSharp/CSharp Winform/Project/Tools/Selenium/2022/Valorant NM/Main version/NightMarketVal/NightMarketVal/Form1.cs
@@ -0,0 +1,472 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Threading;
+
+namespace NightMarketVal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ChromeDriver driver;
+        ChromeOptions options;
+        Thread thr;
+
+
+
+
+        //Path
+        string ProfileFolderPath = Application.StartupPath;
+
+        //browserRunning
+        private void browserRunning()
+        {
+            openSelenium(); // chay browser
+            //loginRiotAccount();
+        }
+
+        //openSelenium
+
+        void openSelenium()
+        {
+            try
+            {
+                options = new ChromeOptions();
+
+                //create folder
+                if (!Directory.Exists(ProfileFolderPath))
+                {
+                    //tao folder moi
+                    Directory.CreateDirectory(ProfileFolderPath);
+                }
+
+                if (Directory.Exists(ProfileFolderPath))
+                {
+                    int count = 1;
+                    options.AddArgument("user-data-dir=" + ProfileFolderPath + "\\User_" + count);
+                    options.AddArgument(@"profile-directory=" + "User_" + count); //chose profile
+
+                }
+
+                //hide console
+                //old path
+                //ChromeDriverService service = ChromeDriverService.CreateDefaultService(@"D:\Code Developer\CSharp\Winforms\Project 2022\ChromeDriver\102.0.5005.63");
+
+                //D:\Code Developer\CSharp\Remake\CSharp Winform\Project\Tools\Selenium\ChromeDriver
+
+                //new path
+                ChromeDriverService service = ChromeDriverService.CreateDefaultService(@"D:\Code Developer\CSharp\Remake\CSharp Winform\Project\Tools\Selenium\ChromeDriver\102.0.5005.63");
+                service.HideCommandPromptWindow = true;
+
+
+                //
+
+
+                //options.AddArgument("headless"); //hide browser
+
+                //driver = new ChromeDriver(@"D:\Code Developer\CSharp\Winforms\Project 2022\ChromeDriver", options);
+                //driver = new ChromeDriver(@"D:\Code Developer\CSharp\Winforms\Project 2022\ChromeDriver\102.0.5005.63", options);
+                //driver.Manage().Window.Position = new Point(3000, 3000);
+                //driver = new ChromeDriver(options);
+
+                //options.AddArgument("--window-position=-32000, -32000");
+
+                //driver = new ChromeDriver(service, options);
+                driver = new ChromeDriver(service);
+
+               
+
+                driver.Url = "https://vn.ku6110.net/Home/Index";
+
+
+
+
+
+
+
+                //ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+                //var checkConsole = service.HideCommandPromptWindow = false; //hide console
+
+                //driver = new ChromeDriver(service);
+                //openIncognito();
+
+                //driver.Navigate().GoToUrl(txtUrl.Text.Trim());
+                //driver.FindElement(By.XPath("//button[@class='ytp-large-play-button ytp-button']")).Click();
+
+                //Maximize Browser  
+                //driver.Manage().Window.Maximize();
+                //driver.Manage().Window.Minimize();
+                //Thread.Sleep(10000);
+                //driver.Navigate().GoToUrl("https://clubloli.com/v/20675/");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        //loginRiotAccount
+        string user_rito = string.Empty;
+        string pass_riot = string.Empty;
+
+        void loginRiotAccount()
+        {
+            try
+            {
+                if (textUser.Length != 0 && textPwd.Length != 0 && count != 0 || textUser != null && textPwd != null && count != 0)
+                {
+                    for (int i = 0; i < count;)
+                    {
+                        Thread.Sleep(1000); //current 3s
+                        //dang o trang co 1 btn dang nhap -> truy cap trang sign in
+                        if (driver.Url == "https://valorant-tracker.com/discord/auth") //dang o trang dang nhap dis cord co btn
+                        {
+                            Thread.Sleep(1000); //current 3s
+
+                            //an nut
+                            gotoLoginDiscord();
+                            Thread.Sleep(7000); //current 3s
+
+                            //trang rito account 
+                            if (checker2())
+                            {
+                                Thread.Sleep(3000);
+
+                                user_rito = textUser[i];
+                                pass_riot = textPwd[i];
+                                driver.FindElements(By.XPath("//*[@id=\"__next\"]/div[2]/section/div[2]/div[1]/div[1]/input"))[0].SendKeys(user_rito);
+                                driver.FindElement(By.XPath("//input[@type='password']")).SendKeys(pass_riot); //chay vao password
+                                Thread.Sleep(2000);
+                                driver.FindElement(By.XPath("//*[@id=\"__next\"]/div[2]/section/div[2]/div[4]/button")).Click();
+                                Thread.Sleep(8000);
+
+                                //check dang nhap
+                                if (driver.Url == "https://valorant-tracker.com/")
+                                {
+
+                                    //vao trang nuon cho no gat
+                                    driver.Navigate().GoToUrl("https://valorant-tracker.com/store/night-market"); //vao luon night market
+
+                                    //kiem tra xem vao dc k
+                                    if (driver.Url == "https://valorant-tracker.com/store/night-market")
+                                    {
+                                        Thread.Sleep(2000);
+
+                                        //lay du lieu trong url
+                                        getDataFromUrl(user_rito);
+
+                                        //cho no ra ngoai de vao acc tiep theo
+                                        driver.Navigate().GoToUrl("https://valorant-tracker.com/discord/auth"); //vao luon night market
+                                        i++;
+                                    }
+                                }
+                                else{ MessageBox.Show("Tài khoản hoặc mật khẩu không chính xác, vui lòng kiểm tra lại.", "Thông báo");}
+                            }
+                            else { MessageBox.Show("Lỗi không phải trang đăng nhập... vui lòng  thử lại", "Thông báo"); }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        //gotoLoginDiscord
+        void gotoLoginDiscord()
+        {
+            try
+            {
+                driver.FindElement(By.XPath("//*[@id=\"__next\"]/div[2]/section/div[2]/div[3]/button")).Click();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //getDataFromUrl
+
+        private void getDataFromUrl(string user_rito)
+        {
+            string[] percent = new string[10];
+            int[] percentInt = new int[10]; //su dung de check > 35%
+            string[] cost = new string[10];
+            string[] name = new string[10];
+
+            //lay acc tren n %
+            //int valuePercent = 35; //35%
+            int valuePercent = int.Parse(txtPercent.Text.Trim());
+
+            string info_account = string.Empty;
+            string info_account2 = string.Empty;
+
+            for (int i = 0; i < 6; i++)
+            {
+                IJavaScriptExecutor js = driver as IJavaScriptExecutor;
+
+                var dataFromJS_Percent = (string)js.ExecuteScript("var sale = document.getElementsByClassName('font-georama ml-2 text-white')[" + i + "].innerHTML;return sale;");
+
+                //xoa dau -, %
+                String strClean = dataFromJS_Percent;
+                percent[i] = strClean.Replace("-", "").Replace("%", "");
+
+                var dataFromJS_Cost = (string)js.ExecuteScript("var cost = document.getElementsByClassName('text-white font-georama p-2')[" + i + "].children[0].innerHTML; return cost;");
+                cost[i] = dataFromJS_Cost;
+
+                var dataFromJS_Name = (string)js.ExecuteScript("var name = document.getElementsByClassName('font-inter uppercase font-bold text-xs text-white')[" + i + "].innerHTML; return name;");
+                name[i] = dataFromJS_Name;
+
+                //MessageBox.Show(percent[i]);
+
+                info_account = user_rito + "[" + i + "]" + "\tGiảm[" + i + "]: " + percent[i] + " Giá[" + i + "]: " + cost[i] + " Name[" + i + "]: " + name[i];
+
+                //ep kieu % so sanh
+                //int isPercent = int.Parse(percent[i]);
+                if (int.Parse(percent[i]) >= valuePercent)
+                {
+                    info_account2 = user_rito + "\t" + percent[i] + "\t[" + cost[i] + "] => " + name[i];
+                    write_file_Account(info_account2, "Valorant_Filter");
+                }
+
+                write_file_Account(info_account, "Valorant_Account_NM");
+            }
+        }
+
+    
+
+
+        //write_file_Account
+        void write_file_Account(string info_Acc, string path)
+        {
+            //write to file
+            string path_passw = Application.StartupPath + "\\" + path + ".txt";
+            //MessageBox.Show(path_passw);
+            try
+            {
+                StreamWriter sw = new StreamWriter(path_passw, true);
+                if (!String.IsNullOrEmpty(info_Acc))
+                {
+                    sw.WriteLine(info_Acc);
+                    sw.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //checker2
+        private bool checker2()
+        {
+            if (driver.Url.Contains("https://valorant-tracker.com/auth"))
+                return true;
+            return false;
+        }
+
+        //tao mang
+        string[] textUser = new string[200]; //toi da 200
+        string[] textPwd = new string[200]; //toi da 200
+        int count = 0;
+
+        private void btnAcc_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            string filePath = string.Empty;
+            string fileExt = string.Empty;
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+
+                filePath = dlg.FileName;
+                fileExt = Path.GetExtension(filePath);
+
+                if (fileExt.CompareTo(".txt") == 0)
+                {
+                    try
+                    {
+                        StreamReader reader = new StreamReader(filePath);
+                        StringBuilder builder = new StringBuilder();
+                        string line = string.Empty;
+                        string text = string.Empty;
+
+                        int i = 0;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            builder.AppendLine(line);
+                            textUser[i] = builder.ToString().Trim();
+                            builder.Clear(); //clear text da 
+                            count++;
+                            i++;
+                        }
+                        //thr = new Thread(browserRunning);
+                        //thr.Start();
+
+                        //MessageBox.Show(builder.ToString());
+                        reader.Close();
+                        lbAcc.Text = Path.GetFileName(filePath);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //xu ly phan %
+            txtPercent.ReadOnly = true;
+            if(int.Parse(txtPercent.Text.Trim()) > 34 && int.Parse(txtPercent.Text.Trim()) < 51)
+            {
+                //phan xu ly file
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                string filePath = string.Empty;
+                string fileExt = string.Empty;
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = dlg.FileName;
+                    fileExt = Path.GetExtension(filePath);
+
+                    if (fileExt.CompareTo(".txt") == 0)
+                    {
+                        try
+                        {
+                            StreamReader reader = new StreamReader(filePath);
+                            StringBuilder builder = new StringBuilder();
+                            string line = string.Empty;
+                            string text = string.Empty;
+                            int i = 0;
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                                builder.AppendLine(line);
+                                textPwd[i] = builder.ToString().Trim();
+                                builder.Clear(); //clear text da 
+                                count++;
+                                i++;
+                            }
+                            thr = new Thread(browserRunning);
+                            thr.Start();
+
+                            reader.Close();
+                            lbPass.Text = Path.GetFileName(filePath);
+                            txtPercent.ReadOnly = false;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+
+                }
+            }
+            else {
+                MessageBox.Show("Lỗi (Giá trị % >= 35% &  Giá trị % <= 50%) => Vui lòng nhập lại!", "Thông báo");
+                txtPercent.ReadOnly = false;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //String newStr = "haduc123-%";
+            //String newStr2 = newStr.Replace("-%", "");
+            //MessageBox.Show(newStr2);
+
+
+            //String newStr2 = "123";
+            //int a = int.Parse(newStr2);
+            //MessageBox.Show((a + 5).ToString());
+
+            //write_file_Account("abc", "Valorant_Filter");
+            //write_file_Account("abc", "Valorant_Account_NM");
+
+
+            thr = new Thread(browserRunning);
+            thr.Start();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Verify that the pressed key isn't CTRL or any non-numeric digit
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // If you want, you can allow decimal (float) numbers
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtPercent.Text = "35";
+        }
+
+        private void txtPercent_Click(object sender, EventArgs e)
+        {
+            txtPercent.ReadOnly = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //neu co driver -> thoat driver
+            if (driver != null)
+            {
+                try
+                {
+                    driver.Dispose();
+                    driver.Quit();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (driver != null)
+            {
+                try
+                {
+                    driver.Dispose();
+                    driver.Quit();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
